35f79a9c15fc412d6a770de807ca16425ef084e4
Bundaberg-Joey <calumhand94@hotmail.com>
2023-03-10 10:36:34 +0000

    fix to alow some quick RF screenings

--- main_reference.py
+++ main_reference.py
@@ -1,4 +1,5 @@
 import argparse
+from uuid import uuid4
 
 import pandas as pd
 import numpy as np
@@ -17,11 +18,12 @@ parser.add_argument('-r', type=str, help='Ranker to use')
 args = parser.parse_args()
 
 ranker_choice = args.r
+run_code = F'{ranker_choice}_{uuid4().hex[::4]}'
 
 # ----------------------------------------------------
 
-X_ref = Hdf5Dataset('Ex7_01_physical.hdf5')
-y_ref = Hdf5Dataset('Ex7_02_selectivity.hdf5')[:].ravel()
+X_ref = Hdf5Dataset('Ex7_05_descriptors.hdf5')
+y_ref = Hdf5Dataset('Ex7_05_selectivity.hdf5')[:].ravel()
 
 greedy_n_ranker = PosteriorRanker(
     model=DenseGaussianProcessregressor(data_set=X_ref),
@@ -37,7 +39,8 @@ ei_ranker = ExpectedImprovementRanker(
 ranker = {'ei': ei_ranker, 'greedy': greedy_n_ranker}[ranker_choice]
 
 
-X_train_ind = list(pd.read_csv('Ex7_03_sample_indices.txt', header=None)[0])
+X_train_ind = np.random.choice(len(y_ref.ravel()), size=1, replace=False)
+#X_train_ind = list(pd.read_csv('Ex7_05_indices_sampled.txt', header=None)[0])
 y_train = y_ref[X_train_ind]
 
 
@@ -53,6 +56,6 @@ for itr in range(370):
     y_train = abs(y_ref[X_train_ind])
     
     df = pd.DataFrame({'x': X_train_ind, 'y': y_train.ravel()})
-    df.to_csv(F'ami_ref_run_{ranker_choice}.csv', index=False)
+    df.to_csv(F'ami_ref_run_{run_code}.csv', index=False)
     
     
