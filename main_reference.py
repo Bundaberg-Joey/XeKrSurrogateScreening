f0e7c0023ff7b6f45fbdb78690fc297c511c40bd
calum hand <crh53@bath.ac.uk>
2023-03-22 19:47:57 +0000

    local changes

--- main_reference.py
+++ main_reference.py
@@ -4,20 +4,20 @@ from uuid import uuid4
 import pandas as pd
 import numpy as np
 
-from surrogate.acquisition import GreedyNRanking, EiRanking
+from surrogate.acquisition import EiRanking
 from surrogate.dense import DenseGaussianProcessregressor, DenseRandomForestRegressor
 from surrogate.data import Hdf5Dataset
 
-from ranking_models import PosteriorRanker, ExpectedImprovementRanker
+from ranking_models import ExpectedImprovementRanker
 
 
 # ----------------------------------------------------
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', type=str, help='Ranker to use')
+parser.add_argument('-m', type=str, help='model to use')
 args = parser.parse_args()
 
-ranker_choice = args.r
+ranker_choice = args.m
 run_code = F'{ranker_choice}_{uuid4().hex[::4]}'
 
 # ----------------------------------------------------
@@ -25,22 +25,19 @@ run_code = F'{ranker_choice}_{uuid4().hex[::4]}'
 X_ref = Hdf5Dataset('Ex7_05_descriptors.hdf5')
 y_ref = Hdf5Dataset('Ex7_05_selectivity.hdf5')[:].ravel()
 
-greedy_n_ranker = PosteriorRanker(
-    model=DenseGaussianProcessregressor(data_set=X_ref),
-    acquisitor=GreedyNRanking(n_opt=100),
-    n_post=100
-    )
-
-ei_ranker = ExpectedImprovementRanker(
+rf_ranker = ExpectedImprovementRanker(
     model=DenseRandomForestRegressor(data_set=X_ref),
     acquisitor=EiRanking()
 )
 
-ranker = {'ei': ei_ranker, 'greedy': greedy_n_ranker}[ranker_choice]
+gp_ranker = ExpectedImprovementRanker(
+    model=DenseGaussianProcessregressor(data_set=X_ref),
+    acquisitor=EiRanking()
+)
 
+ranker = {'rf': rf_ranker, 'gp': gp_ranker}[ranker_choice]
 
-X_train_ind = list(np.random.choice(len(y_ref.ravel()), size=1, replace=False))
-#X_train_ind = list(pd.read_csv('Ex7_05_indices_sampled.txt', header=None)[0])
+X_train_ind = list(pd.read_csv('Ex7_05_indices_sampled.txt', header=None)[0])
 y_train = y_ref[X_train_ind]
 
 
